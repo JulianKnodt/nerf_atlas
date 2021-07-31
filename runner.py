d4c9212cd4c94f585ac274c5dcd7269158c7de2b
Joe Bartusek <jfb4@tigercpu.princeton.edu>
2021-07-31 15:53:47 -0400

    new flag for making new directory

--- runner.py
+++ runner.py
@@ -235,6 +235,10 @@ def arguments():
 
   args = a.parse_args()
 
+  if args.timed_outdir:
+    args.outdir = os.path.join(args.outdir, datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
+  if not os.path.exists(args.outdir): os.mkdir(args.outdir)
+
   # runtime checks
   hyper_config.load(args)
   if args.timed_outdir:
@@ -685,9 +689,6 @@ def seed(s):
 def main():
   args = arguments()
   seed(args.seed)
-  outdir = os.path.join(args.outdir, datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
-  os.mkdir(outdir)
-  args.outdir = outdir
 
   labels, cam, light = loaders.load(args, training=True, device=device)
   if args.train_imgs > 0:
